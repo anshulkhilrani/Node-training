2d17877e243e7a870b54b397a6fc84cc57b00350
anshulkhilrani <khilranianshul@gmail.com>
2022-09-20 12:05:34 +0530

    google 0Auth 2.0

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express')
+const passport = require('passport')
 const app = express()
+const GoogleStrategy = require('passport-google-oauth20').Strategy
 
 const PORT = process.env.PORT || 5000
+
+passport.use(new GoogleStrategy())
+
+
 
 app.get('/', (req, res) => {
     res.send({hi: "the"})
