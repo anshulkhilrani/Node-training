0b4d38048685c2b4bbf3c89c49f18a86e5b26538
anshulkhilrani <khilranianshul@gmail.com>
2022-09-22 14:21:47 +0530

    using chalk lib

--- notes-app/index.js
+++ notes-app/index.js
@@ -1,12 +1,8 @@
+const chalk = require('chalk')
 const getNotes = require('./notes.js')
 
 const notes = getNotes()
 
 console.log(notes)
 
-
-// const add = require('./utils.js')
-
-// const sum = add(4 , 2)
-
-// console.log(sum)
+console.log(chalk.green.inversew('Success!'))
