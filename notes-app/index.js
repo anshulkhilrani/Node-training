ba0908aea1f6836cd1d7d9138d97a22154457609
anshulkhilrani <khilranianshul@gmail.com>
2022-09-23 11:18:29 +0530

    using command line args with yarg

--- notes-app/index.js
+++ notes-app/index.js
@@ -1,8 +1,54 @@
 const chalk = require('chalk')
+const yargs = require('yargs')
 const getNotes = require('./notes.js')
 
-const notes = getNotes()
+yargs.version('1.1.0')
 
-console.log(notes)
+// add, remove, read, list
 
-console.log(chalk.green.inversew('Success!'))
+// create add command
+
+yargs.command({
+    command: 'add',
+    describe: 'Add a new Note!',
+    handler: function () {
+        console.log("add a new note")
+    }
+})
+
+
+// create remove command
+
+yargs.command({
+    command: 'remove',
+    describe: 'remove a new Note!',
+    handler: function () {
+        console.log("remove a note")
+    }
+})
+
+
+// create read command
+
+yargs.command({
+    command: 'read',
+    describe: 'Read a Note!',
+    handler: function () {
+        console.log("Read a new note")
+    }
+})
+
+
+// create list command
+
+yargs.command({
+    command: 'list',
+    describe: 'List all of the notes!',
+    handler: function () {
+        console.log("Listing all the notes.")
+    }
+})
+
+
+
+console.log(yargs.argv)
